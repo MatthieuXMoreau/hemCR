04cd63d8a3f3bf19df4b489d1b54d42e0181ce4f
MatthieuXMoreau <matthieu.moreau@inserm.fr>
2022-06-13 16:05:09 +0200

    Allow grouping cells by genotype

--- Functions/functions_GeneTrends.R
+++ Functions/functions_GeneTrends.R
@@ -6,16 +6,23 @@ trend <- function(Seurat.data,
                   group.by,
                   gene){
   
-  data <- Seurat.data@meta.data %>% select(Lineage, Pseudotime, Phase)
+  data <- Seurat.data@meta.data %>% select(Lineage, Pseudotime, Phase, orig.ident)
   data$Gene <- Seurat.data@assays$RNA@data[gene,]
   
-  if (!group.by == "Lineage") {
+  if (group.by == "Lineage") {
     p <- ggplot(data=data, aes(x= Pseudotime, y= Gene)) +
       geom_point(aes(color= Phase), size=0.5) +
       scale_color_manual(values= c(wes_palette("FantasticFox1")[1:3],"grey40",wes_palette("FantasticFox1")[5])) +
       geom_smooth(method="loess", n= 50, fill="grey") +
       ylim(0,NA) +
       ggtitle(gene)
+  } else if (group.by == "Genotype") {
+    data$Genotype_Lineage <- paste0(data$orig.ident, " ", data$Lineage)
+    p <- ggplot(data=data, aes(x= Pseudotime, y= Gene)) +
+      scale_color_manual(values= c(wes_palette("FantasticFox1")[1:3],"grey40",wes_palette("FantasticFox1")[5])) +
+      geom_smooth(method="loess", n= 50, aes(color= Genotype_Lineage, linetype= orig.ident)) +
+      ylim(0,NA) +
+      ggtitle(gene)  
   } else {
     p <- ggplot(data=data, aes(x= Pseudotime, y= Gene)) +
       geom_point(aes(color= Lineage), size=0.5) +
